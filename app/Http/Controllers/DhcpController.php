a72df1e110f9063959766e2004ac8d44eb4cc939
David Silva <antoniopedrosa.silva@gmail.com>
2025-04-09 15:00:47 +0100

    dhcp done

--- app/Http/Controllers/DhcpController.php
+++ app/Http/Controllers/DhcpController.php
@@ -6,13 +6,16 @@ use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 use GuzzleHttp\Client;
 use Illuminate\Support\Str;
+use GuzzleHttp\Exception\RequestException;
 
 class DhcpController extends Controller
 {
 
     public function index(): View
     {
-        $client = new Client();
+        $client = new Client([
+            'verify' => false
+        ]);
         $res = $client->get('http://' . session('address') . '/rest/ip/dhcp-server', ['auth' =>  [session('username'), session('password')]]);
 
         return view('dhcp.index')->with('data', json_decode($res->getBody()));
@@ -43,6 +46,53 @@ class DhcpController extends Controller
 
     }
 
+    public function downloadDhcp()
+    {
+        $client = new Client([
+            'verify' => false
+        ]);
+        $res = $client->get('https://' . session('address') . '/rest/ip/dhcp-server', ['auth' =>  [session('username'), session('password')]]);
+
+        $tempFilePath = storage_path('app/temp.json');
+        file_put_contents($tempFilePath, $res->getBody());
+
+        // Return the file as a downloadable response
+        return response()->download($tempFilePath, 'DhcpServer.json')->deleteFileAfterSend(true);
+
+    }
+
+
+
+    public function createDhcp()
+    {
+
+        $client = new Client([
+            'verify' => false
+        ]);
+
+        $resInt = $client->get('https://' . session('address') . '/rest/interface', ['auth' =>  [session('username'), session('password')]]);
+        $interfacesName = [];
+        $interfaces = json_decode($resInt->getBody());
+        foreach($interfaces as $interface){
+            
+            $interfacesName[] = $interface->name;
+        }
+
+        $resPool = $client->get('https://' . session('address') . '/rest/ip/pool', ['auth' =>  [session('username'), session('password')]]);
+        $poolName = [];
+
+        $pools = json_decode($resPool->getBody());
+
+        foreach($pools as $pool){
+            
+            $poolName[] = $pool->name;
+        }
+        
+        return view('dhcp.create')->with('interfaces', $interfacesName)->with('pools',$poolName);
+
+    }
+
+
     public function createDhcpPool()
     {
         return view('dhcp.createPool');
@@ -65,6 +115,37 @@ class DhcpController extends Controller
         return view('dhcp.editPool')->with('dhcpPool', $res)->with('rangeBegin', $ranges[0])->with('rangeEnd', $ranges[1]);
     }
 
+    public function editDhcp(string $id)
+    {
+        $client = new Client([
+            'verify' => false
+        ]);
+
+        $res = $client->get('https://' . session('address') . '/rest/ip/dhcp-server/' . $id, ['auth' =>  [session('username'), session('password')]]);
+
+        $res = json_decode($res->getBody());
+
+        $resInt = $client->get('https://' . session('address') . '/rest/interface', ['auth' =>  [session('username'), session('password')]]);
+        $interfacesName = [];
+        $interfaces = json_decode($resInt->getBody());
+        foreach($interfaces as $interface){
+            
+            $interfacesName[] = $interface->name;
+        }
+
+        $resPool = $client->get('https://' . session('address') . '/rest/ip/pool', ['auth' =>  [session('username'), session('password')]]);
+        $poolName = [];
+
+        $pools = json_decode($resPool->getBody());
+
+        foreach($pools as $pool){
+            
+            $poolName[] = $pool->name;
+        }
+
+        return view('dhcp.edit')->with('dhcp', $res)->with('interfaces', $interfacesName)->with('pools',$poolName);
+    }
+
     public function storeDhcpPool(Request $request)
     {
 
@@ -83,8 +164,68 @@ class DhcpController extends Controller
 
         $res = $client->put('https://' . session('address') . '/rest/ip/pool', ['auth' =>  [session('username'), session('password')],
                             'json' => ['name' => $name, 'ranges' => $ranges ]]);
-
+        
         return redirect()->route('showDhcpPool')->with('success', 'DHCP updated successfully!');
+
+    }
+
+    public function storeDhcp(Request $request)
+    {
+
+        $request->validate([
+            'name' => ['required','string'],
+            'interface' => ['required','string'],
+            'address-pool' => ['required','string'],
+        ]);
+
+        $name = $request->input('name');
+        $interface = $request->input('interface');
+        $pool = $request->input('address-pool');
+        $lease = '30m';
+
+        $client = new Client([
+            'verify' => false
+        ]);
+
+        try{
+
+            $res = $client->put('https://' . session('address') . '/rest/ip/dhcp-server', ['auth' =>  [session('username'), session('password')],
+                            'json' => ['name' => $name, 'interface' => $interface, 'address-pool' => $pool, 'lease-time' => $lease ]]);
+
+        } catch( RequestException $e) {
+            return back()->withErrors(['global' => $e->getMessage()])->withInput();
+        }
+        return redirect()->route('showDhcp')->with('success', 'DHCP created successfully!');
+
+    }
+
+    public function updateDhcp(Request $request, string $id)
+    {
+
+        $request->validate([
+            'name' => ['required','string'],
+            'interface' => ['required','string'],
+            'address-pool' => ['required','string'],
+        ]);
+
+        $name = $request->input('name');
+        $interface = $request->input('interface');
+        $pool = $request->input('address-pool');
+
+        $client = new Client([
+            'verify' => false
+        ]);
+        try{
+
+            $res = $client->patch('https://' . session('address') . '/rest/ip/dhcp-server/' . $id, ['auth' =>  [session('username'), session('password')],
+                            'json' => ['name' => $name, 'interface' => $interface, 'address-pool' => $pool ]]);
+                            
+        } catch( RequestException $e) {
+            return back()->withErrors(['global' => $e->getMessage()])->withInput();
+        }
+        
+
+        return redirect()->route('showDhcp')->with('success', 'DHCP updated successfully!');
 
     }
 
@@ -118,6 +259,16 @@ class DhcpController extends Controller
         ]);
         $res = $client->delete('https://' . session('address') . '/rest/ip/pool/' .$id, ['auth' =>  [session('username'), session('password')]]);
 
-        return redirect()->route('showDnsStatic')->with('success', 'Data deleted successfully!');
+        return redirect()->route('showDhcpPool')->with('success', 'Data deleted successfully!');
+    }
+
+    public function destroyDhcp(string $id)
+    {
+        $client = new Client([
+            'verify' => false
+        ]);
+        $res = $client->delete('https://' . session('address') . '/rest/ip/dhcp-server/' .$id, ['auth' =>  [session('username'), session('password')]]);
+
+        return redirect()->route('showDhcp')->with('success', 'Data deleted successfully!');
     }
 }
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -123,7 +123,7 @@
                     <!-- Dropdown Menu -->
                     <ul id="dropdown-dhcp" class="hidden py-2 space-y-2">
                         <li>
-                            <a href="#" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">DHCP</a>
+                            <a href="{{route('showDhcp')}}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">DHCP</a>
                         </li>
                         <li>
                             <a href="{{route('showDhcpPool')}}" class="flex items-center w-full p-2 text-gray-900 transition duration-75 rounded-lg pl-11 group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700">DHCP Pool</a>
