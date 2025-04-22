29c8b35a39384cdd676cc6b06fc352dd870e4c8f
jgrabovschi <iorgusor@gmail.com>
2025-04-22 15:57:33 +0100

    Update label text for frequency selection in wireless config

--- resources/views/wireless/config.blade.php
+++ resources/views/wireless/config.blade.php
@@ -70,7 +70,7 @@
             </select>
         </div>
         <div class="mb-5">
-            <label for="frequency" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a mode</label>
+            <label for="frequency" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select a frequency</label>
             <select name="frequency" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                 @foreach ($frequencies as $frequency)
                 <option {{ $frequency == $wlan->{'frequency'} ? 'selected' : ''}} value="{{ $frequency }}">
